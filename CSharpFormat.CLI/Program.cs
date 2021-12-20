76bb54211bb859aa53a6ac2d0c4b9d68c2fc2494
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-12-20 16:20:15 +0000

    - build fix

--- CSharpFormat.CLI/Program.cs
+++ CSharpFormat.CLI/Program.cs
@@ -1,5 +1,7 @@
 ﻿namespace CSharpFormat.CLI
 {
+  using System;
+  using System.IO;
   using Manoli.Utils.CSharpFormat;
 
   public static class Program
@@ -10,7 +12,7 @@
       var srcFmt = (Manoli.Utils.CSharpFormat.CSharpFormat)CodeFormatFactory.Create(SourceLanguages.CSharp);
       using var strm = File.Open(csFilePath, FileMode.Open);
       var htmlSrc = srcFmt.FormatCode(strm);
-      
+
       Console.WriteLine(csFilePath);
       Console.WriteLine();
       Console.WriteLine(htmlSrc);
